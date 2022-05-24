ac509c31ff7a201fc613e81604853ac278a944f9
Leonardo <leocurty14@gmail.com>
2022-05-24 03:53:52 -0300

    Primeiro Commit

---
+++ app/src/main/java/com/example/easytask/ui/AddTaskActivity.java
@@ -0,0 +1,17 @@
+package com.example.easytask.ui;
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.dio.todolist.databinding.ActivityAddTaskBinding
+import br.com.dio.todolist.datasource.TaskDataSource
+import br.com.dio.todolist.extensions.format
+import br.com.dio.todolist.extensions.text
+import br.com.dio.todolist.model.Task
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
+import java.util.*
+
+public class AddTaskActivity {
+}
---
+++ app/src/main/java/com/example/easytask/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.easytask.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.easytask.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
